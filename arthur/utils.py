a2374ae85da2ad8041165893db4cf0514e458c5f
Joe Banks <joe@jb3.dev>
2021-07-17 21:29:22 +0100

    chore: add monty python reference to error message

--- arthur/utils.py
+++ arthur/utils.py
@@ -4,7 +4,7 @@ from discord import Embed
 from discord.colour import Colour
 
 
-def generate_error_embed(title: str = "Failed!", description: str = "An error occurred") -> Embed:
+def generate_error_embed(title: str = "'Tis but a scratch!", description: str = "An error occurred") -> Embed:
     """Generate an error embed to return to Discord."""
     return Embed(
         title=title,
