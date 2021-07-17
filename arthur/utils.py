69f42c451ec5a5eb204c8617251ecc372b262411
Joe Banks <joe@jb3.dev>
2021-07-17 21:29:43 +0100

    fix: linting compliance

--- arthur/utils.py
+++ arthur/utils.py
@@ -4,10 +4,8 @@ from discord import Embed
 from discord.colour import Colour
 
 
-def generate_error_embed(title: str = "'Tis but a scratch!", description: str = "An error occurred") -> Embed:
+def generate_error_embed(
+    title: str = "'Tis but a scratch!", description: str = "An error occurred"
+) -> Embed:
     """Generate an error embed to return to Discord."""
-    return Embed(
-        title=title,
-        description=description,
-        colour=Colour.red()
-    )
+    return Embed(title=title, description=description, colour=Colour.red())
