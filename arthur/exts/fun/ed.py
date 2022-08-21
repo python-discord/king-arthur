eb3d54b387ccf5c74a5045a7bfa47d9b82229910
Chris Lovering <chris.lovering.95@gmail.com>
2022-08-21 22:17:23 +0100

    Use the correct help kwarg for ed
    This was updated in Discord.py 2.0

--- arthur/exts/fun/ed.py
+++ arthur/exts/fun/ed.py
@@ -11,8 +11,8 @@ class Ed(Cog):
     def __init__(self, bot: KingArthur) -> None:
         self.bot = bot
 
-    @command(name="ed", usage="[-GVhs] [-p string] [file]")
-    async def ed(self, ctx: Context, *, _args: str) -> None:
+    @command(name="ed", help="[-GVhs] [-p string] [file]")
+    async def ed(self, ctx: Context) -> None:
         """Ed is the standard text editor."""
         await ctx.send("?")
 
