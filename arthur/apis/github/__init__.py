23c21269f7edcf7b197449afd9c12ef69a224bed
Joe Banks <joe@jb3.dev>
2025-07-10 14:28:36 +0100

    Typo on GitHubException import

--- arthur/apis/github/__init__.py
+++ arthur/apis/github/__init__.py
@@ -1,3 +1,3 @@
-from .teams import GitHubError, add_staff_member
+from .teams import GitHubException, add_staff_member
 
-__all__ = ("GitHubError", "add_staff_member")
+__all__ = ("GitHubException", "add_staff_member")
