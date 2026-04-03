56a0f0233aed5d8fc5f0ba14949b3d1dc991f380
Chris Lovering <chris.lovering.95@gmail.com>
2026-04-03 21:45:51 +0100

    Comment out helpers for now
    Otherwise helpers would get enrolled to LDAP and we're not ready for that yet

--- arthur/constants.py
+++ arthur/constants.py
@@ -13,7 +13,7 @@ class LDAPGroupMapping(TypedDict):
 # This is a mapping of LDAP groups to Discord role IDs and GitHub team IDs. It is used to determine
 # which users should be eligible for LDAP enrollment.
 LDAP_ROLE_MAPPING: dict[str, LDAPGroupMapping] = {
-    "helpers": {"discord_role_id": 267630620367257601, "github_team_slug": "helpers"},
+    # "helpers": {"discord_role_id": 267630620367257601, "github_team_slug": "helpers"},  # noqa: ERA001
     "devops": {"discord_role_id": 409416496733880320, "github_team_slug": "devops"},
     "administrators": {"discord_role_id": 267628507062992896, "github_team_slug": "admins"},
     "moderators": {"discord_role_id": 267629731250176001, "github_team_slug": "moderators"},
@@ -24,4 +24,4 @@ LDAP_ROLE_MAPPING: dict[str, LDAPGroupMapping] = {
 
 # Users are only checked for enrollment if they have this role. This doesn't grant them any
 # permissions, it is for performance to avoid iterating roles for every other user in the guild.
-HELPER_ROLE_ID = LDAP_ROLE_MAPPING["helpers"]["discord_role_id"]
+HELPER_ROLE_ID = 267630620367257601  # LDAP_ROLE_MAPPING["helpers"]["discord_role_id"]
