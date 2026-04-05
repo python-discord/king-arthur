ea782c1139c25cf765b1b491b8fb710fa55b2542
Joe Banks <joe@jb3.dev>
2026-04-05 02:52:47 +0100

    Remove manual GitHub management commands

--- arthur/exts/github/management.py
+++ arthur/exts/github/management.py
@@ -2,57 +2,23 @@
 
 from typing import TYPE_CHECKING
 
-from discord.ext.commands import Cog, Context, group
-
-from arthur.apis.github import GitHubError, add_member_to_team, remove_org_member
-from arthur.config import CONFIG
-from arthur.constants import LDAP_ROLE_MAPPING
+from discord.ext import tasks
+from discord.ext.commands import Cog
 
 if TYPE_CHECKING:
     from arthur.bot import KingArthurTheTerrible
 
 
 class GitHubManagement(Cog):
-    """Ed is the standard text editor."""
+    """GitHub organisation membership synchronisation with LDAP."""
 
-    def __init__(self, bot: KingArthurTheTerrible) -> None:
+    def __init__(self, bot: "KingArthurTheTerrible") -> None:
         self.bot = bot
 
-    async def cog_check(self, ctx: Context) -> bool:
-        """Check if the user has permission to use this cog."""
-        return (
-            CONFIG.admins_role in [r.id for r in ctx.author.roles]
-            or CONFIG.devops_role in [r.id for r in ctx.author.roles]
-            or await self.bot.is_owner(ctx.author)
-        )
+    @tasks.loop(minutes=10)
+    async def sync_github_org(self) -> None:
+        """Synchronise GitHub organisation membership with LDAP."""
 
-    @group(name="github", invoke_without_command=True)
-    async def github(self, ctx: Context) -> None:
-        """Group of commands for managing the GitHub organisation."""
-        if ctx.invoked_subcommand is None:
-            await ctx.send_help(ctx.command)
-
-    @github.command(name="add")
-    async def add_team_member(self, ctx: Context, username: str) -> None:
-        """Add a user to the default GitHub team."""
-        try:
-            await add_member_to_team(username, LDAP_ROLE_MAPPING["helpers"]["github_team_slug"])
-            await ctx.send(f":white_check_mark: Successfully invited {username} to the staff team.")
-        except GitHubError as e:
-            await ctx.send(f":x: Failed to add {username} to the staff team: {e}")
-
-    @github.command(name="remove")
-    async def remove_org_member(self, ctx: Context, username: str) -> None:
-        """Remove a user from the GitHub organisation."""
-        try:
-            await remove_org_member(username)
-            await ctx.send(
-                f":white_check_mark: Successfully removed {username} from the GitHub organisation."
-            )
-        except GitHubError as e:
-            await ctx.send(f":x: Failed to remove {username} from the organisation: {e}")
-
-
-async def setup(bot: KingArthurTheTerrible) -> None:
+async def setup(bot: "KingArthurTheTerrible") -> None:
     """Add cog to bot."""
     await bot.add_cog(GitHubManagement(bot))
